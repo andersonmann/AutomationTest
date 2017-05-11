b3186f13ae2508b1442b244a4ef4421aba29a448
andersonm <andersonm@DBS-0618.dbserver.com.br>
2017-05-11 18:11:50 -0300

    tests with JUnit

--- OnlineStore/src/junitTest/SeleniumTest.java
+++ OnlineStore/src/junitTest/SeleniumTest.java
@@ -3,8 +3,14 @@ package junitTest;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
+	
+	
+	
+	
 
 	@Test
 	public void test() {
