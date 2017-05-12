19c75ea6a48645eb17b690dbfa10811ea83f35be
andersonm <andersonm@DBS-0618.dbserver.com.br>
2017-05-12 17:32:19 -0300

    Junit Test with Selenium WebDriver, done.

--- OnlineStore/src/junitTest/SeleniumTest.java
+++ OnlineStore/src/junitTest/SeleniumTest.java
@@ -12,41 +12,57 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
-	
+
 	static String browser = "webdriver.chrome.driver";
 	static String path = "C:\\Program Files\\Java\\chrome\\chromedriver.exe";
-	WebElement element;
+	//WebDriver driver;
 	
-	WebDriver driver;
+	private static ChromeDriver driver;
+	WebElement element;
 
 	@BeforeClass
 	public static void openBrowser() {
 		System.setProperty(browser, path);
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void valid_UserCredential() {
-		System.setProperty(browser, path);
-		WebDriver driver = new ChromeDriver();
 
 		System.out.println("Starting test " + new Object() {
 		}.getClass().getEnclosingMethod().getName());
 		driver.get("http://www.store.demoqa.com");
 		driver.findElement(By.xpath(".//*[@id='account']/a")).click();
-		driver.findElement(By.id("log")).sendKeys("testuser_3");
-		driver.findElement(By.id("pwd")).sendKeys("Test@123");
+		driver.findElement(By.id("log")).sendKeys("anderson");
+		driver.findElement(By.id("pwd")).sendKeys("escola3092");
 		driver.findElement(By.id("login")).click();
 		try {
 			element = driver.findElement(By.xpath(".//*[@id='account_logout']/a"));
 		} catch (Exception e) {
-
 		}
 		Assert.assertNotNull(element);
 		System.out.println("Ending test " + new Object() {
 		}.getClass().getEnclosingMethod().getName());
+	}
 
+	@Test
+	public void invalid_UserCredential() {		
+
+		System.out.println("Starting test " + new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		driver.get("http://www.store.demoqa.com");
+		driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+		driver.findElement(By.id("log")).sendKeys("anderson");
+		driver.findElement(By.id("pwd")).sendKeys("escola3092@123");
+		driver.findElement(By.id("login")).click();
+		try {
+			element = driver.findElement(By.xpath(".//*[@id='account_logout']/a"));
+		} catch (Exception e) {
+		}
+		Assert.assertNotNull(element);
+		System.out.println("Ending test " + new Object() {
+		}.getClass().getEnclosingMethod().getName());
 	}
 
 	@AfterClass
